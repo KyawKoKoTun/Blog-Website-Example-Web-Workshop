10f4bbb95f13d8a77c92543a34d93d97bd216c4d
Kyaw Ko Ko Tun <kyawkokotunmm475157@gmail.com>
2024-08-01 21:59:31 +0630

    Added CORS

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import *
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(
+    app,
+    resources={r"/*": {"origins": "*"}},
+    supports_credentials=True,
+    allow_headers="*",
+    methods=["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+)
 posts = [
-    {'id':'1', "title": "Hello World", "content": "This is the first post"},
-    {'id':'1', "title": "Second Post", "content": "This is the second post"},
+    {"id": "1", "title": "Hello World", "content": "This is the first post"},
+    {"id": "1", "title": "Second Post", "content": "This is the second post"},
 ]
 
 
